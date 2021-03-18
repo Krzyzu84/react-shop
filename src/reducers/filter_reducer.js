e08e23dc7f321ae1f7002c97d222d836484cb54f
Krzyzu84 <krzysztofptak97@gmail.com>
2021-03-18 20:01:12 +0100

    Sort added

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -8,9 +8,49 @@ import {
   FILTER_PRODUCTS,
   CLEAR_FILTERS,
 } from '../actions'
+import products_reducer from './products_reducer'
 
 const filter_reducer = (state, action) => {
-  return state
+  if(action.type === LOAD_PRODUCTS){
+    return {
+      ...state,
+      all_products: [...action.payload],
+      filtered_products: [...action.payload],
+    }
+  }
+  if(action.type === SET_GRIDVIEW){
+    return {...state, grid_view:true}
+  }
+  if(action.type === SET_LISTVIEW){
+    return {...state, grid_view:false}
+  }
+  if(action.type === UPDATE_SORT){
+    return {...state, sort: action.payload}
+  }
+  if(action.type === SORT_PRODUCTS){
+    const {sort, filtered_products} = state;
+    let tempProduct = [...filtered_products];
+    if(sort === 'price-lowest'){
+      tempProduct = tempProduct.sort((a,b) =>
+      a.price - b.price)
+    }
+    if(sort === 'price-highest'){
+      tempProduct = tempProduct.sort((a,b) =>
+      b.price - a.price)
+    }
+    if(sort === 'name-a'){
+      tempProduct = tempProduct.sort((a,b) =>{
+        return a.name.localeCompare(b.name)
+      })
+    }     
+    if (sort === 'name-z'){
+      tempProduct = tempProduct.sort((a,b) =>{
+        return b.name.localeCompare(a.name)
+      })     
+  }
+    return {...state, filtered_products: tempProduct }
+  }
+
   throw new Error(`No Matching "${action.type}" - action type`)
 }
 
